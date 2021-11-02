da53a26d86cc8d0ff79eb8dd95bacf6408aba264
mazen <mak9986@hotmail.com>
2021-11-02 12:50:59 +1100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (driverAB) => {
+    const people = driverAB[0] driverAB[1];
+    return people;
+};
+
+const returnLastTwoDrivers = (d) => {
+    const people = [d[d.length-2], [d[d.length-1]];
+    return people;
+}
+
+const selectingDrivers = [returnLastTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = () => {
+    return function(a){
+        return a * a;
+    }
+}
+
+const fairDoubler =(b)=> {
+    return b * 2;
+}
+
+const fareTripler = (a)=> {
+    return a * 3;
+}
+
+const selectDifferentDrivers=(a,b) =>{
+    return b(a);
+}
